fa56e2eaf0e3d653607ff0d810c0dd8127457320
Osama Bsharat <bsharat1999@gmail.com>
2024-03-13 15:17:46 +0200

    tab-bar-commit

--- lib/screen/homepage.dart
+++ lib/screen/homepage.dart
@@ -10,31 +10,62 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            "facebook",
-            style: TextStyle(
-              fontSize: 25,
-              fontWeight: FontWeight.bold,
-              color: Colors.blue,
-            ),
-          ),
-          actions: <Widget>[
-            IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.search,
-                color: Colors.black,
+    return DefaultTabController(
+      length: 5,
+      child: Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              "facebook",
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+                color: Colors.blue,
               ),
             ),
-            IconButton(
+            actions: <Widget>[
+              IconButton(
                 onPressed: () {},
-                icon: Icon(Icons.message, color: Colors.black)),
-          ],
-        ),
-        body: TabBarView(children: <Widget>[
-          Icon(Icons.home),
-        ]));
+                icon: Icon(
+                  Icons.search,
+                  color: Colors.black,
+                ),
+              ),
+              IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.message,
+                    color: Colors.black,
+                  )),
+            ],
+            bottom: TabBar(
+                unselectedLabelColor: Colors.black,
+                labelColor: Colors.blue,
+                indicatorColor: Colors.blue,
+                tabs: [
+                  Tab(
+                    icon: Icon(Icons.home),
+                  ),
+                  Tab(
+                    icon: Icon(Icons.ondemand_video),
+                  ),
+                  Tab(
+                    icon: Icon(Icons.group),
+                  ),
+                  Tab(
+                    icon: Icon(Icons.notifications),
+                  ),
+                  Tab(
+                    icon: Icon(Icons.menu),
+                  ),
+                ]),
+          ),
+          body: TabBarView(children: <Widget>[
+            Icon(Icons.home),
+            Icon(Icons.home),
+            Icon(Icons.home),
+            Icon(Icons.home),
+            Icon(Icons.home),
+          ])),
+    );
   }
 }
